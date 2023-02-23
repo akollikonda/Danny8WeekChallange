6b6fc5556979e83b4c90340853f4db6baa349554
Abhilash Kollikonda <101874878+akollikonda@users.noreply.github.com>
2023-02-24 02:37:05 +0530

    Update Week 2 Solution.sql

--- Week2/Week 2 Solution.sql
+++ Week2/Week 2 Solution.sql
@@ -1,3 +1,9 @@
 /* A. Pizza Metrics */
 
 /* 1) How many pizzas were ordered?*/
+
+select count(pizza_id) as pizzas_ordered from customer_orders;
+
+/* 2) How many unique customer orders were made?*/
+
+select count(distinct order_id) as unique_customer_orders from customer_orders;
