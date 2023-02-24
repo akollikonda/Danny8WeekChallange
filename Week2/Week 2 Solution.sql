9572cac028acbd4373125277e73dab172862e11b
Abhilash Kollikonda <101874878+akollikonda@users.noreply.github.com>
2023-02-24 18:45:53 +0530

    Update Week 2 Solution.sql

--- Week2/Week 2 Solution.sql
+++ Week2/Week 2 Solution.sql
@@ -2,8 +2,57 @@
 
 /* 1) How many pizzas were ordered?*/
 
-select count(pizza_id) as pizzas_ordered from customer_orders;
+select 
+  count(pizza_id) as pizzas_ordered 
+ from customer_orders;
 
 /* 2) How many unique customer orders were made?*/
 
-select count(distinct order_id) as unique_customer_orders from customer_orders;
+select 
+  count(distinct order_id) as unique_customer_orders 
+ from customer_orders;
+
+/* 3) How many successful orders were delivered by each runner?*/
+
+select 
+	runner_id, 
+	count(order_id) as successful_orders 
+from runner_orders 
+where cancellation is null or cancellation like ""
+group by runner_id
+order by runner_id;
+    
+/* 4) How many of each type of pizza was delivered?*/
+
+select 
+	pizza_id,
+  count(order_id) as num_of_pizzas
+from customer_orders
+group by pizza_id;
+  
+/* 5) How many Vegetarian and Meatlovers were ordered by each customer?*/
+
+select 
+	customer_id,
+    sum(case when pizza_name='Meatlovers' then 1
+    else 0 
+    end) as Meatlovers,
+    sum(case when pizza_name='Vegetarian' then 1
+    else 0 
+    end) as Vegetarian
+from customer_orders co 
+join pizza_names pn on co.pizza_id=pn.pizza_id
+group by customer_id;
+
+/* 6) What was the maximum number of pizzas delivered in a single order?*/
+
+select 
+	order_id, 
+  count(pizza_id) as Max_pizzas_delievered
+from customer_orders 
+group by order_id 
+order by count(pizza_id) desc 
+limit 1;
+
+/* 7) For each customer, how many delivered pizzas had at least 1 change and how many had no changes?*/
+
